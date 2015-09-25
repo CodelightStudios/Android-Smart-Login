261f5e27583756b74015324cd2286b8313f84c65
Kalyan Dechiraju <kalyandechiraju@outlook.com>
2015-09-25 13:52:56 +0530

    Implement Parcelable for SmartFacebookUser Class

--- smartloginlibrary/src/main/java/studios/codelight/smartloginlibrary/users/SmartFacebookUser.java
+++ smartloginlibrary/src/main/java/studios/codelight/smartloginlibrary/users/SmartFacebookUser.java
@@ -1,14 +1,51 @@
 package studios.codelight.smartloginlibrary.users;
 
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by Kalyan on 9/23/2015.
  */
-public class SmartFacebookUser extends SmartUser {
+public class SmartFacebookUser extends SmartUser implements Parcelable {
     private String profileName;
     private String middleName;
     private Uri profileLink;
+
+    public SmartFacebookUser() {
+    }
+
+    protected SmartFacebookUser(Parcel in) {
+        super(in);
+        profileName = in.readString();
+        middleName = in.readString();
+        profileLink = in.readParcelable(Uri.class.getClassLoader());
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(profileName);
+        dest.writeString(middleName);
+        dest.writeParcelable(profileLink, flags);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<SmartFacebookUser> CREATOR = new Creator<SmartFacebookUser>() {
+        @Override
+        public SmartFacebookUser createFromParcel(Parcel in) {
+            return new SmartFacebookUser(in);
+        }
+
+        @Override
+        public SmartFacebookUser[] newArray(int size) {
+            return new SmartFacebookUser[size];
+        }
+    };
 
     public String getProfileName() {
         return profileName;
